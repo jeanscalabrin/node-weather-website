f9b9838f246973e5a979204d33a3230c8a2e4cb2
Jean Scalabrin <jscalabrin94@gmail.com>
2022-12-12 15:21:48 -0300

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Arquivo Javascript do lado do cliente')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
